c865ef9c317cfe2ba38b86c51e0e864d5c61fcde
abduttawab <abduttawabcse@gmail.com>
2019-10-27 00:06:47 +0600

    first commit

---
+++ CoreTest_WebApi/Controllers/ApplicationUserController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CoreTest_WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CoreTest_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+    
+        private readonly ApplicationSettings _appSettings;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IOptions<ApplicationSettings> appSettings)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST
+        public async Task<object> PostApplicationUser(ApplicationUserModel applicationUserModel)
+        {
+            applicationUserModel.Role = "Admin";
+            var appUser = new ApplicationUser()
+            {
+                UserName = applicationUserModel.UserName,
+                Email = applicationUserModel.Email,
+                FullName = applicationUserModel.FullName
+            };
+            try
+            {
+
+                var result = await _userManager.CreateAsync(appUser, applicationUserModel.Password);
+                await _userManager.AddToRoleAsync(appUser, applicationUserModel.Role);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login (LoginModel loginModel)
+
+        {
+            var user = await _userManager.FindByNameAsync(loginModel.UserName);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
+            {
+                //Get Roles
+                var role = await _userManager.GetRolesAsync(user);
+                IdentityOptions _options = new IdentityOptions();
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[] {
+                        new Claim("UserID",user.Id.ToString()),
+                        new Claim(_options.ClaimsIdentity.RoleClaimType,role.FirstOrDefault())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+                var token = tokenHandler.WriteToken(securityToken);
+                return Ok(new { token });
+            }
+            else
+                return BadRequest(new { message = "Username or Password is incorrect." });
+
+
+
+        }
+
+    }
+}
---
+++ CoreTest_WebApi/Controllers/UserProfileController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreTest_WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreTest_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        public UserProfileController(UserManager<ApplicationUser> userManager) {
+
+            _userManager = userManager;
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        //[Route("Register")]
+        //POST
+        public async Task<object> GetUserProfile()
+        {
+            string userId = User.Claims.First(m => m.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+
+            return new
+            {
+
+                user.FullName,
+                user.Email,
+                user.UserName
+            };
+        }
+
+        [HttpGet]
+        [Authorize(Roles ="Admin")]
+        [Route("ForAdmin")]
+     
+        public string GetForAdmin()
+        {
+
+            return "Admin Only";
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Customer")]
+        [Route("ForCustomer")]
+
+        public string GetForCustomer()
+        {
+
+            return "Customer Only";
+        }
+
+    }
+}
